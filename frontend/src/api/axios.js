cd3e20702401276dfd93a237c916778956cc5646
Harshavardhan <dharshavardhan52@gmail.com>
2026-05-04 15:23:11 +0530

    databasemerge

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "http://127.0.0.1:8000/api/";
-const baseURL = "http://54.183.89.14/api/";
+const baseURL = "http://127.0.0.1:8000/api/";
+// const baseURL = "http://54.183.89.14/api/";
 
 console.log("API Base URL:", baseURL);
 
